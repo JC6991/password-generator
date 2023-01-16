493166e79808b8f6dee26fdef4fb81533b4c7134
Joshua Crehan <joshua.crehan@yahoo.com>
2023-01-16 10:11:16 +0000

    create prompts to get user selections

--- script.js
+++ script.js
@@ -88,8 +88,51 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+// Object to store user input for password options
+let passwordOptions = {
+  passwordLength: 0,
+  lowercaseChar:  true,
+  uppercaseChar: true,
+  numericChar: true,
+  specialChar: true
+}
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  passwordOptions.passwordLength = parseInt(prompt("Please state the character length of your desired password between 10 and 64 characters:"));  
+  
+  if(passwordOptions.passwordLength >= 10 && passwordOptions.passwordLength <= 64){
+
+    passwordOptions.lowercaseChar = confirm("Please click confirm if you want to include lowercase characters in your passwors, otherwise click cancel.");    
+
+      if(passwordOptions.lowercaseChar || passwordOptions.lowercaseChar === false){
+
+        passwordOptions.uppercaseChar = confirm("Please click confirm if you want to include uppercase characters in your passwors, otherwise click cancel.");        
+
+          if(passwordOptions.uppercaseChar || passwordOptions.uppercaseChar === false){
+
+            passwordOptions.numericChar = confirm("Please click confirm if you want to include numeric characters in your passwors, otherwise click cancel.");            
+
+            if(passwordOptions.numericChar || passwordOptions.numericChar === false){
+
+              passwordOptions.specialChar = confirm("Please click confirm if you want to include special characters in your passwors, otherwise click cancel.");              
+
+              if(passwordOptions.specialChar || passwordOptions.specialChar === false){
+                
+              };
+            };
+          };
+      };
+  }
+  else if(passwordOptions.passwordLength === false){
+
+  }
+  else{
+    alert("Invalid input!! You need to choose a value between 10 and 64.");
+    getPasswordOptions();
+  };
+     
+  return;
 
 }
 
@@ -100,6 +143,17 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
+
+  getPasswordOptions();
+
+  // if statement to capture if user makes no selections
+  if((passwordOptions.lowercaseChar === false &&
+    passwordOptions.uppercaseChar === false &&
+    passwordOptions.numericChar === false &&
+    passwordOptions.specialChar === false) ) {
+      alert("You must confirm at least ONE password option.");
+      getPasswordOptions();
+    }
 
 }
 
